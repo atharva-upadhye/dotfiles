df7ccf632428845f03627140d7b18845e556cb1d
Atharva Upadhye <70993547+atharva-upadhye@user.noreply.github.com>
2026-01-11 23:02:44 +0530

    add: nixos from scratch config with flake

---
+++ nixos/flake.nix
@@ -0,0 +1,29 @@
+
+{
+	description = "NixOS from Scratch";
+	inputs = {
+		nixpkgs.url = "nixpkgs/nixos-25.11";
+		home-manager = {
+			url = "github:nix-community/home-manager/release-25.11";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+	outputs = { self, nixpkgs, home-manager, ... }: {
+		nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
+			system = "aarch64-darwin";
+			modules = [
+				./configuration.nix
+				home-manager.nixosModules.home-manager
+				{
+					home-manager = {
+						useGlobalPkgs = true;
+						useUserPackages = true;
+						users.atharva = import ./home.nix;
+						backupFileExtension = "backup";
+					};
+				}
+			];
+		};
+	};
+}
+
---
+++ nixos/home.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }: {
+	home.username = "atharva";
+	home.homeDirectory = "/home/atharva";
+	home.stateVersion = "25.11";
+	home.packages = with pkgs; [
+		neovim
+	];
+	programs.brave = {
+		enable = true;
+		package = pkgs.brave;
+		# extensions = [ { id = ""; }];
+	};
+	programs.git.enable = true;
+	programs.bash = {
+		enable = true;
+		shellAliases = {
+			btw = "echo I use nixos, btw";
+		};
+	}; 
+}
+
