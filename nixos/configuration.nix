c84f262430c42ce367db41cd85d92ac73dcdae93
atharva-upadhye <70993547+atharva-upadhye@user.noreply.github.com>
2026-01-22 01:50:48 +0530

    nixos: add nodejs_20 for remote server

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -81,6 +81,7 @@
    # vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
    # wget
 	  git
+    nodejs_20
   ];
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
