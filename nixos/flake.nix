3ddb6107457ea54ff775429052d701c56e42c4be
atharva-upadhye <70993547+atharva-upadhye@user.noreply.github.com>
2026-01-23 11:45:10 +0530

    refactor(nixos): reorganize configuration and migrate user settings to separate file

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,29 +1,42 @@
-
 {
-	description = "NixOS from Scratch";
-	inputs = {
-		nixpkgs.url = "nixpkgs/nixos-25.11";
-		home-manager = {
-			url = "github:nix-community/home-manager/release-25.11";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-	};
-	outputs = { self, nixpkgs, home-manager, ... }: {
-		nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
-			system = "aarch64-darwin";
-			modules = [
-				./configuration.nix
-				home-manager.nixosModules.home-manager
-				{
-					home-manager = {
-						useGlobalPkgs = true;
-						useUserPackages = true;
-						users.atharva = import ./home.nix;
-						backupFileExtension = "backup";
-					};
-				}
-			];
-		};
-	};
-}
+  description = "NixOS from Scratch";
+  inputs = {
+    home-manager = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager/release-25.11";
+    };
+    nixpkgs.url = "nixpkgs/nixos-25.11";
+  };
+  outputs =
+    {
+      home-manager,
+      nixpkgs,
+      self,
+      ...
+    }:
+    {
+      nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              backupFileExtension = "backup";
+              useGlobalPkgs = true;
+              users.atharva = import ./home.nix;
+              useUserPackages = true;
+            };
+          }
+        ];
+        system = "aarch64-darwin";
+      };
 
+      # Standalone Home Manager configuration (no sudo needed)
+      homeConfigurations.atharva = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          system = "aarch64-linux";
+        };
+        modules = [ ./home.nix ];
+      };
+    };
+}
--- nixos/home.nix
+++ nixos/home.nix
@@ -1,42 +1,63 @@
-{ config, pkgs, ... }: {
-	home.username = "atharva";
-	home.homeDirectory = "/home/atharva";
-	home.stateVersion = "25.11";
-	home.packages = with pkgs; [
-		neovim
-	];
-	programs.alacritty = {
-		enable = true;
-		# This wraps the binary with the environment variable
-		# doing this is important since GPU config is not working out of the box for alacritty
-		package = (pkgs.alacritty.overrideAttrs (oldAttrs: {
-			nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ pkgs.makeWrapper ];
-			postInstall = (oldAttrs.postInstall or "") + ''
-			wrapProgram $out/bin/alacritty \
-				--set LIBGL_ALWAYS_SOFTWARE 1
-			'';
-		}));
-		
-		settings = {
-			# Your usual alacritty settings here
-			font.size = 10;
-		};
-	};
-	programs.brave = {
-		enable = true;
-		package = pkgs.brave;
-		# extensions = [ { id = ""; }];
-	};
-	programs.git.enable = true;
-	programs.bash = {
-		enable = true;
-		shellAliases = {
-			btw = "echo I use nixos, btw";
-		};
-	};
-	xresources.properties = {
-		"XTerm*background" = "black";
-		"XTerm*foreground" = "white";
-	};
-}
+{
+  config,
+  pkgs,
+  ...
+}:
+{
+  home = {
+    homeDirectory = "/home/atharva";
+    packages = with pkgs; [
+      neovim
+      nixfmt
+      # nvim
+      tree
+      # vim
+      # wget
+    ];
+    stateVersion = "25.11";
+    username = "atharva";
+  };
+  programs = {
+    alacritty = {
+      enable = true;
+      # This wraps the binary with the environment variable
+      # doing this is important since GPU config is not working out of the box for alacritty
+      package = (
+        pkgs.alacritty.overrideAttrs (oldAttrs: {
+          nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ pkgs.makeWrapper ];
+          postInstall = (oldAttrs.postInstall or "") + ''
+            wrapProgram $out/bin/alacritty \
+            	--set LIBGL_ALWAYS_SOFTWARE 1
+          '';
+        })
+      );
 
+      settings = {
+        # Your usual alacritty settings here
+        font = {
+          size = 10;
+        };
+      };
+    };
+    bash = {
+      enable = true;
+      shellAliases = {
+        btw = "echo I use nixos, btw";
+      };
+    };
+    brave = {
+      enable = true;
+      package = pkgs.brave;
+      # extensions = [ { id = ""; }];
+    };
+    git = {
+      enable = true;
+    };
+  };
+  xresources = {
+    properties = {
+      "XTerm*background" = "black";
+      "XTerm*foreground" = "white";
+    };
+  };
+}
